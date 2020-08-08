f088d9218039a9ca364954fb2e72d883ffa363c0
karam1984 <zkaramzawad@hotmail.com>
2020-08-08 13:13:43 +0200

    karamWeek4

---
+++ week4/my-app/src/components/Form.js
@@ -0,0 +1,14 @@
+  
+import React from 'react';
+import Button from './Button';
+import Input from './Input';
+
+
+export default function Form({ city, setCity, onSubmit }) {
+  return (
+    <form>
+      <Input city={city} setCity={setCity} />
+      <Button onSubmit={onSubmit} />
+    </form>
+  );
+} 
